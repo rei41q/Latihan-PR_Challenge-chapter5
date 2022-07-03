656305d6083460a9594f0abc0a51965a56a2c118
Rei Greiq <greiqrei@gmail.com>
2022-07-04 02:46:30 +0800

    update URL

--- PR/app.js
+++ PR/app.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const app = express();
 const port = 8007;
-const userList = [];
+const userList = [{
+   id: "1",
+   nama: "greiq",
+   password: "12345"
+},{
+  id: "2",
+  nama: "greiq 2",
+  password: "12345"
+}
+,{
+  id: "3",
+  nama: "greiq 3",
+  password: "12345"
+}];
 const userId = 0;
 app.use(express.json());
 
 app.get('/', (req , res)=>{
-    res.send("hi there from binar")
+    return res.send("hi there from binar")
 })
 
 app.post('/users/daftarakun', function(req, res, next) { //DAFTAR AKUN
@@ -14,26 +27,26 @@ app.post('/users/daftarakun', function(req, res, next) { //DAFTAR AKUN
     const user = req.body['nama'];
     const pass = req.body['password'];
     userList.push({"id": id, "nama" : user, "password": pass})
-    res.send("berhasil daftar akun")
+    return res.send("berhasil daftar akun")
   });
 
   app.get('/users/list', (req,res)=> { // LIST SEMUA YG SDH DIDAFTARKAN
-    res.json(userList);
+    return res.json(userList);
   })
 
   app.get("/users/:userID", (req,res) =>{ //GET 1 USER BERDASARKAN ID 1,2,3,4...
     const index = req.params.userID;
-    res.json(userList[index-1]);
+    return res.json(userList[index-1]);
 });
   
-app.put("/updateUsers/:userID", (req,res) =>{ //UPDATE USER BERDASARKAN ID 1,2,3,4...
+app.put("/Users/:userID", (req,res) =>{ //UPDATE USER BERDASARKAN ID 1,2,3,4...
     const index = req.params.userID-1;
     userList[index].nama =  req.body['nama'];
     userList[index].password = req.body['password'];
-    res.send("berhasil update nama dan password")
+    return res.send("berhasil update nama dan password")
 });
 
-app.delete('/deleteUsers/:userID',(req, res) =>{ //DELETE USER BERDASARKAN ID 1,2,3,4...
+app.delete('/Users/:userID',(req, res) =>{ //DELETE USER BERDASARKAN ID 1,2,3,4...
     const index = req.params.userID-1;
     console.log(index);
     userList.splice(index, 1);
