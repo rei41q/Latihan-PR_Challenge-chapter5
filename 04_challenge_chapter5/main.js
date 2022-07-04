ba08b58a0327af5719cdf885e163fb9787704254
Rei Greiq <greiqrei@gmail.com>
2022-07-04 17:13:45 +0800

    update main.js

--- 04_challenge_chapter5/main.js
+++ 04_challenge_chapter5/main.js
@@ -4,11 +4,6 @@ const userRouter = require('./users/users.logic');
 const app = express()
 const port = 8000
 
-
-// !important! 
-// you need to install the following libraries |express|[dotenv > if required]
-// or run this command >> npm i express dotenv 
-
 app.use(express.json());
 
 app.use(express.static(__dirname + '/public'))
